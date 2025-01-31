4918da5a2eddbcd32413f8c72635c88f5ebfc451
Gabriel da Costa Rodrigues <167185964+GRodrigues10@users.noreply.github.com>
2025-01-31 16:13:53 -0300

    Update index.jsx

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -9,7 +9,8 @@ export function Home(){
                 <div className="elipse-container">
 
                     <div className="elipse-left">
-                        <Elipse/>
+                        <Elipse size={120} opacity={0.2} style={circle-left-one}/>
+                        <Elipse size={180} opacity={0.4} style={circle-left-two}/>
                     </div>
 
                     <div className="elipse-right"></div>
